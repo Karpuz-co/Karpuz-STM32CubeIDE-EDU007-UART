cd6e56428db01eb1de269e7c563b7ebd4056e52e
Fouad Aladhami <fouadiadhami@gmail.com>
2022-06-27 12:46:52 +0300

    import libraries for sprintf function

--- EDU007-UART/Core/Src/main.c
+++ EDU007-UART/Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
+#include "string.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
